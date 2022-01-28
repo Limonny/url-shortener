8fcc4e27b15495a8043e3b9040167a2c976511a1
Limonny <limonny7@gmail.com>
2022-01-28 12:18:33 +0300

    api docs added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, password, role, status)
+VALUES ('user@mail.com', '$2a$12$VSEmah/.3ClrcW./uGxB0.4GDKrXW.gcDTUhmFujUznMy8mOaspja', 'USER', 'ACTIVE'),
+       ('admin@mail.com', '$2a$12$Hdkzd48/v8bABtUBA/Fbvu.u5cmCz6n1KAq/NGAa/pxSYMc.c4GvC', 'ADMIN', 'ACTIVE');
